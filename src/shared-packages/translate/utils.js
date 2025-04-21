c33e3c7ec873a0a9fcb84da39f523c5ad3c6f494
Alex Oliynyk <asashayko@gmail.com>
2025-04-21 14:06:12 +0100

    add possibility to auto paste selected text

--- src/shared-packages/translate/utils.js
+++ src/shared-packages/translate/utils.js
@@ -1,10 +1,16 @@
-import { toLower } from "lodash";
+import { toLower, find } from "lodash";
 export function shortHash(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = (hash << 5) - hash + str.charCodeAt(i);
-    hash = hash & hash;
-  }
-  return hash.toString(32);
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = (hash << 5) - hash + str.charCodeAt(i);
+        hash = hash & hash;
+    }
+    return hash.toString(32);
 }
 export const createTranslationKey = (fromTo, text) => shortHash(`${fromTo}-${toLower(text)}`);
+export const getTranslatedSentence = (sentence) => {
+    return find(sentence, (s) => (s === null || s === void 0 ? void 0 : s.hasOwnProperty("orig")) && s.hasOwnProperty("trans"));
+};
+export const getTranslitSentence = (sentence) => {
+    return find(sentence, (s) => (s === null || s === void 0 ? void 0 : s.hasOwnProperty("translit")) && s.hasOwnProperty("src_translit"));
+};
