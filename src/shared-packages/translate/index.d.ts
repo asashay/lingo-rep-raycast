35ee292e80a82e99de58345196cc617273fbb188
Alex Oliynyk <asashayko@gmail.com>
2025-04-22 10:40:33 +0100

    fix lint

--- src/shared-packages/translate/index.d.ts
+++ src/shared-packages/translate/index.d.ts
@@ -1,19 +1,24 @@
 import { googleTranslationSchema, GoogleTranslation } from "./googleTranslationSchema";
 import { createTranslationKey, getTranslatedSentence, getTranslitSentence } from "./utils";
-export declare const translate: (from: string, to: string, text: string, context?: string) => Promise<TranslatedText | null>;
+export declare const translate: (
+  from: string,
+  to: string,
+  text: string,
+  context?: string,
+) => Promise<TranslatedText | null>;
 type TranslatedText = {
-    translation: GoogleTranslation & {
-        aiTranslation?: {
-            translation: string;
-            collocationWords: string;
-            collocationWordsTranslation?: string;
-        };
+  translation: GoogleTranslation & {
+    aiTranslation?: {
+      translation: string;
+      collocationWords: string;
+      collocationWordsTranslation?: string;
     };
-    context: string;
-    text: string;
-    timestamp: number;
-    fromTo: string;
-    hashKey: string;
+  };
+  context: string;
+  text: string;
+  timestamp: number;
+  fromTo: string;
+  hashKey: string;
 };
 export type { GoogleTranslation, TranslatedText };
-export { googleTranslationSchema, createTranslationKey, getTranslatedSentence, getTranslitSentence, };
+export { googleTranslationSchema, createTranslationKey, getTranslatedSentence, getTranslitSentence };
--- src/shared-packages/translate/utils.d.ts
+++ src/shared-packages/translate/utils.d.ts
@@ -2,5 +2,7 @@ import { GoogleTranslation, translationSchema, translitSchema } from "./googleTr
 import { z } from "zod";
 export declare function shortHash(str: string): string;
 export declare const createTranslationKey: (fromTo: string, text: string) => string;
-export declare const getTranslatedSentence: (sentence: GoogleTranslation["sentences"]) => z.infer<typeof translationSchema>;
+export declare const getTranslatedSentence: (
+  sentence: GoogleTranslation["sentences"],
+) => z.infer<typeof translationSchema>;
 export declare const getTranslitSentence: (sentence: GoogleTranslation["sentences"]) => z.infer<typeof translitSchema>;
